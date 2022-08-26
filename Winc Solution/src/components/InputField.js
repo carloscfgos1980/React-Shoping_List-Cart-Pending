d49e718340c83940b18480f03ff2288ed0d5a5c9
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-26 06:01:35 +0200

    Add files via upload

---
+++ Winc Solution/src/components/InputField.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function InputField({ onSubmit }) {
+    const [inputValue, setInputValue] = useState("");
+
+    function onButtonPress() {
+        if (inputValue.length > 0) {
+            console.log(`Submitting ${inputValue}`);
+            onSubmit(inputValue);
+            setInputValue("");
+        }
+    }
+
+    return (
+        <div className="input-field">
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={onButtonPress} placeholder="Add a grocery item">
+                Add
+            </button>
+        </div>
+    );
+}
+
+export default InputField;
